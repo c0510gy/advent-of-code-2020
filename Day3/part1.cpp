d95062469131a05eb1421ba1ce783440db07c86e
Sang-geon Yun <c0510gy@protonmail.ch>
2021-01-14 12:25:59 +0900

    solution for day3 part1

---
+++ Day3/part1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  freopen("./input.txt", "r", stdin);
+  int ans = 0, x = 0, y = 0;
+  for(string line; getline(cin, line); ++y, x += 3){
+    int n = line.size();
+    if(line[x % n] == '#')
+      ++ans;
+  }
+  cout << ans << "\n";
+  return 0;
+}
